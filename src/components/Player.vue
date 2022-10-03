2b77a1e1576a593554bd810f49ddd36ed3d1c0cf
James Walker <james.chunho@gmail.com>
2022-10-03 01:22:39 +0100

    Create Player component

---
+++ src/components/Player.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {ref, watch} from 'vue';
+
+const props = defineProps({
+  // the video this player is currently playing
+  src: String,
+  // the type of video this player is currently playing, e.g. 'video/mp4'
+  type: String,
+});
+
+const videoEl = ref(null);
+
+// watch the props.src variable
+// if it changed, we manually reload the video
+watch(() => props.src, (newVal, oldVal) => {
+  // do nothing if the video element doesn't exist yet (may happen on first startup)
+  if (videoEl.value === null) return;
+
+  // call #load on the video element
+  videoEl.value.load();
+});
+
+function seekTo(time) {
+  videoEl.value.currentTime = time;
+}
+
+function play() {
+  videoEl.value.play();
+}
+
+function pause() {
+  videoEl.value.pause();
+}
+
+// expose the #seekTo method to parents
+defineExpose({seekTo, play, pause});
+</script>
+
+<template>
+  <video controls ref="videoEl">
+    <source :src="props.src" :type="props.type">
+  </video>
+</template>
+
+<style scoped>
+video {
+  width: 100%;
+}
+</style>
