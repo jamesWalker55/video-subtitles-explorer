ac8d670adc6ff2774ed681b2a11e0ce0d2d1be9c
James Walker <james.chunho@gmail.com>
2022-10-06 00:51:33 +0100

    Add vite-svg-loader to load svgs
    svg loader

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
 
   // Vite optons tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
