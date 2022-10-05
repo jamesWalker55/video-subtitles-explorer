79ea22e0ddce821669b6a9b43a4bc0d5b3d925bc
James Walker <james.chunho@gmail.com>
2022-10-05 12:55:16 +0100

    Expose functions locate_vtt, read_vtt to view

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,15 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod vtt;
+mod vtt_locator;
+
+use vtt::read_vtt;
+use vtt_locator::locate_vtt;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![locate_vtt, read_vtt])
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+        .expect("error while running tauri appl}
