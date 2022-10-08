72e7a514796c543fc23d4e3d1036dc5a852f6d39
James Walker <james.chunho@gmail.com>
2022-10-08 13:59:19 +0100

    Implement #scrollToIndex for CueDisplay

--- src/components/CueDisplay.vue
+++ src/components/CueDisplay.vue
@@ -1,5 +1,6 @@
 <script setup>
 import CueItem from './CueItem.vue';
+import {defineExpose, ref} from 'vue';
 
 const props = defineProps({
   // the list of cues to be rendered
@@ -9,12 +10,35 @@ const props = defineProps({
 });
 
 const emit = defineEmits(['seek']);
+
+const displayEl = ref(null);
+
+function scrollToIndex(index) {
+  const cueEl = displayEl.value.querySelector(`[data-index="${index}"]`);
+  if (cueEl === null) return;
+
+  const viewportHeight = displayEl.value.clientHeight;
+  // .offsetTop returns the position relative to the main window for some reason
+  // i'm subtracting it by the parent's .offsetTop to get the position relative to the scroll area
+  const cueLocalOffsetTop = cueEl.offsetTop - displayEl.value.offsetTop;
+  const cueHeight = cueEl.getBoundingClientRect().height;
+
+  const targetYpos = cueLocalOffsetTop - (viewportHeight - cueHeight) / 2;
+
+  displayEl.value.scrollTo({top: targetYpos})
+}
+
+defineExpose({scrollToIndex});
 </script>
 
 <template>
-  <ul id="cue-display" class="overflow-auto h-full">
+  <ul id="cue-display" class="overflow-auto h-full" ref="displayEl">
     <template v-for="(cue, index) in props.cues">
-      <CueItem :cue="cue" :is-current="index === props.currentIndex" @seek="(...args) => emit('seek', ...args)"/>
+      <CueItem
+          :cue="cue"
+          :is-current="index === props.currentIndex"
+          :data-index="index"
+          @seek="(...args) => emit('seek', ...args)"/>
     </template>
   </ul>
 </template>
