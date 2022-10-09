2e89b481ea262120223a8089f1f1de4fc5337efd
James Walker <james.chunho@gmail.com>
2022-10-09 14:57:42 +0100

    Disable selecting in the toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="h-10 bg-gray-900 flex flex-row text-white items-center p-2 gap-2">
+  <div class="h-10 bg-gray-900 flex flex-row text-white items-center p-2 gap-2 select-none">
     <slot/>
   </div>
 </template>
